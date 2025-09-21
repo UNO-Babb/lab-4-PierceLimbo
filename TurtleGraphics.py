c7a801322a12440da248e6a4c1fc14c0c06fa196
Pierce Limbo <plimbo@unomaha.edu>
2025-09-21 15:19:37 -0500

    Update TurtleGraphics.py

--- TurtleGraphics.py
+++ TurtleGraphics.py
@@ -1,27 +1,94 @@
 #TurtleGraphics.py
-#Name:
-#Date:
-#Assignment:
+#Name: Pierce Limbo
+#Date: 9/22/2025
+#Assignment: Lab3 
 
-import turtle #needed generally but not in CodeHS
-hideturtle() #hides the default turtle in CodeHS
+#=======================
+#Random Polygons
+var MAX_SIZE = 80;
+var MAX_SHAPES = 100;
+var counter = 0;
 
-def drawSquare(myTurtle, size):
-    for i in range(4):
-        myTurtle.forward(size)
-        myTurtle.right(90)
+function start(){
+    setTimer(draw, 50);
+}
+
+function draw(){
+    drawPolygon(Randomizer.nextInt(3, 8),     // random sides (triangle–octagon)
+                Randomizer.nextInt(20, MAX_SIZE), 
+                Randomizer.nextColor(),
+                Randomizer.nextInt(0, getWidth()),
+                Randomizer.nextInt(0, getHeight()));
+    
+    counter++;
+    if(counter == MAX_SHAPES){
+        stopTimer(draw);
+    }
+}
+
+function drawPolygon(sides, radius, color, cx, cy){
+    var poly = new Polygon();
+    for(var i = 0; i < sides; i++){
+        var ang = i * (2 * Math.PI / sides);
+        var x = cx + radius * Math.cos(ang);
+        var y = cy + radius * Math.sin(ang);
+        poly.addPoint(x, y);
+    }
+    poly.setColor(color);
+    add(poly);
+}
+#===========================================
+#random corner
+var MAX_SIZE = 120;
+var MAX_SHAPES = 100;
+var counter = 0;
+
+function start(){
+    setTimer(draw, 50);
+}
+
+function draw(){
+    drawSquareWithCornerFill(Randomizer.nextInt(40, MAX_SIZE),
+                             Randomizer.nextColor(),
+                             Randomizer.nextInt(0, getWidth()),
+                             Randomizer.nextInt(0, getHeight()),
+                             Randomizer.nextInt(1, 4)); 
+    
+    counter++;
+    if(counter == MAX_SHAPES){
+        stopTimer(draw);
+    }
+}
+
+function drawSquareWithCornerFill(size, color, cx, cy, corner){
+    var rect = new Rectangle(size, size);
+    rect.setColor(color);
+    rect.setPosition(cx - size/2, cy - size/2);
+    add(rect);
+
+    var tri = new Polygon();
+    if(corner === 1){ 
+        tri.addPoint(cx - size/2, cy - size/2);
+        tri.addPoint(cx, cy - size/2);
+        tri.addPoint(cx - size/2, cy);
+    } else if(corner === 2){ 
+        tri.addPoint(cx, cy - size/2);
+        tri.addPoint(cx + size/2, cy - size/2);
+        tri.addPoint(cx + size/2, cy);
+    } else if(corner === 3){ 
+        tri.addPoint(cx - size/2, cy);
+        tri.addPoint(cx, cy);
+        tri.addPoint(cx - size/2, cy + size/2);
+    } else { 
+        tri.addPoint(cx, cy);
+        tri.addPoint(cx + size/2, cy);
+        tri.addPoint(cx + size/2, cy + size/2);
+    }
+    tri.setColor(Color.black); 
+    add(tri);
+}
+
+#======================================================
+#random square in squares
 
 
-def main():
-    myTurtle = turtle.Turtle()
-    # drawPolygon(myTurtle, 5) #draws a pentagon
-    # drawPolygon(myTurtle, 8) #draws an octogon
-
-    # fillCorner(myTurtle, 2) #draws a square with top right corner filled in.
-    # fillCorner(myTurtle, 3) #draws a square bottom left corner filled in.
-
-    # squaresInSquares(myTurtle, 5) #draws 5 concentric squares
-    # squaresInSquares(myTurtle, 3) #draws 3 concentric squares
-
-
-main()
